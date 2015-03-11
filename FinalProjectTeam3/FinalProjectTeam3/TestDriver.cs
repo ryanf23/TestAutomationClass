78b6e154b9acaa0af9670671132ffccdc109e647
ryanf23 <rtfarrell23@gmail.com>
2015-03-10 19:55:58 -0700

    Initial Framework

---
+++ FinalProjectTeam3/FinalProjectTeam3/TestDriver.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="TestDriver.cs" company="[YourName]">
+// Copyright © 2014. All rights reserved.
+// </copyright>
+
+namespace YourName.ProjectName
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+    /// <summary>
+    /// This is our poor test suite driver
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Executes all defined test methods
+        /// </summary>
+        static void Main()
+        {
+            // Initializes the test suite
+            TestSuite test = new TestSuite();
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            // Call the test methods
+            test.TestMethod();
+
+            timer.Stop();
+            TimeSpan ts = timer.Elapsed;
+
+            // Format TimeSpan value. 
+            string elapsedTime = string.Format(
+                CultureInfo.InvariantCulture,
+                "{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours,
+                ts.Minutes,
+                ts.Seconds,
+                ts.Milliseconds / 10);
+
+            Console.WriteLine("Total Test Suite Time: {0}", elapsedTime);
+        }
+    }
+}
