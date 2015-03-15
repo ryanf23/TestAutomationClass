8f782bf7de58e9e4d02978547b0d0fa531b58a31
ryanf23 <rtfarrell23@gmail.com>
2015-03-14 23:09:28 -0700

    Added some of the File trees

--- FinalProjectTeam3/FinalProjectTeam3/TEDnotepadHelper.cs
+++ FinalProjectTeam3/FinalProjectTeam3/TEDnotepadHelper.cs
@@ -14,17 +14,21 @@ namespace FinalProjectTeam3
             Cancelbtn = 0x2,
             Filenametxt = 0x47C
         }
-        public enum SaveDialog
+
+        public enum SaveAsDialog
         {
             Textfilecombbx = 0x0,
             Savebtn = 0x1,
             Cancelbtn = 0x2,
             Filenametxt = 0x3E9
         }
+
         public enum TedApplication
         {
+            MainTextEntryField = 0x3FC,
             Statusbar = 0X406
         }
+
         public enum Findblock
         {
             Findtextbox = 0x83E,
@@ -37,6 +41,62 @@ namespace FinalProjectTeam3
             RegExpschbx = 0x842,
             Wraparoundchbx = 0x843,
             Use2ndsearchchbx = 0x844
+        }
+
+        public enum MenuItems
+        {
+            File = 0,
+            Edit = 1,
+            Search = 2,
+            Tools = 3,
+            Favourites = 4,
+            Clips = 5,
+            Options = 6,
+            Help = 7
+        }
+
+        public enum FileMenuItems
+        {
+            New = 0,
+            Open = 1,
+            Revert = 2,
+            Reopen = 3,
+            Import = 4,
+            Include = 5,
+            Save = 7,
+            SaveAs = 8,
+            Export = 9,
+            Exclude = 10,
+            Encoding = 12,
+            NewLines = 13,
+            Properties = 14,
+            PageSetup = 16,
+            Print = 17,
+            RecentFiles = 19,
+            HideinTray = 21,
+            NewWindow = 22,
+            SavenExit = 24,
+            Exit = 25,
+        }
+
+        public enum SearchMenuItems
+        {
+            Find = 0,
+            FindNext = 1,
+            FindPrevious = 2,
+            FindSelected = 3,
+            FindLater = 4,
+            SelecttoNext = 5,
+            SelecttoPrevious = 6,
+            Replace = 8,
+            LastTool = 9,
+            Lookfor = 11,
+            LookforNext = 12,
+            LookforPrevious = 13,
+            LookforSelected = 14,
+            LookforLater = 15,
+            SelecttoNext2 = 16,
+            SelecttoPrevious2 = 17
         }
     }
 }
